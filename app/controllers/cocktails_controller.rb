448cfa20f6431e6ef15e81e6edce91fcac2673a2
theo rouland <theo.rouland@laposte.net>
2019-08-15 18:03:25 +0200

    good 2

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,35 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktails_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    # Use callbacks to share common setup or constraints between actions.
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Dose.destroy_all
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+ing1 = Ingredient.create!(name: "lemon")
+ing2 = Ingredient.create!(name: "ice")
+ing3 = Ingredient.create!(name: "mint leaves")
+
+cocktail1 = Cocktail.create!(name: "Mojito")
+
+Dose.create!(description: "4", cocktail: cocktail1, ingredient: ing1)
+Dose.create!(description: "8", cocktail: cocktail1, ingredient: ing2)
